b9cc364ec09e0cc132f3f2705aba3a3ebecdc30f
farahasha1 <>
2021-06-04 10:13:37 +0000

    add new file

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('WELCOM TO OUR WORLD.')
+
+var answer = prompt ("YOU WANT TO BE ELEGANC")
+
+if(answer == "yes"){confirm("SO FOLLOW US. ")}
+
+else if(answer == "no"){confirm("BELIVE ME,YOU LIKE TO BE HERE" )}
+console.log(answer)
+
+
+document.write("YOU DESERVE US")
