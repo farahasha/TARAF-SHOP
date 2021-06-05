28ead3b69dfe2b1e93f923729170341dda297d9d
farahasha1 <>
2021-06-05 21:09:33 +0000

    add coloun

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ alert('WELCOM TO OUR WORLD.')
 
 var answer = prompt ("YOU WANT TO BE ELEGANC")
 
-if(answer == "yes"){confirm("SO FOLLOW US. ")}
+if(answer == "yes"){
+  confirm("SO FOLLOW US. ")
+  }
 
-else if(answer == "no"){confirm("BELIVE ME,YOU LIKE TO BE HERE" )}
-console.log(answer)
-
-
-document.write("YOU DESERVE US")
+else if(answer == "no"){
+  confirm("BELIVE ME,YOU LIKE TO BE HERE" )
+  }
+else
+{
+ alert("How beautiful do you see yourself out of 10?")
+}
+var pic=prompt("How beautiful do you see yourself out of 10?")
+if (userAnswer <= 10)
+{
+  for ( var i=0 ; i<pic)
+  {
+  }
+console.log[document.write("<https://media.istockphoto.com/vectors/funny-lettering-illustration-of-youre-gorgeous-vector-id1186676882 />" )]
+ 
+  }
