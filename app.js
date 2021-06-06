5e225035056a26788f7c2017378cd16f4d2c010d
farahasha1 <>
2021-06-06 14:39:39 +0000

    add coloun

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ if(answer == "yes"){
 else if(answer == "no"){
   confirm("BELIVE ME,YOU LIKE TO BE HERE" )
   }
-
+function img1(){
 var numberofimg = prompt("How beautiful do you see yourself out of 10?")
 while (numberofimg >= 10){
 numberofimg=prompt("How beautiful do you see yourself out of 10?")
 }
+function img2(){
 for(var i=0;i<numberofimg;i++){
 document.write("<img ='img' src='https://media.istockphoto.com/vectors/funny-lettering-illustration-of-youre-gorgeous-vector-id1186676882' />" )
 } 
-
+}
+}
+console.log('img (1;2)')
