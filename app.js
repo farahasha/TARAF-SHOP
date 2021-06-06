1debc741b31bacca5eba78327f3b3cc833ead193
farahasha1 <>
2021-06-06 16:00:11 +0000

    add coloun

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
+
+function start(){
+
 alert('WELCOM TO OUR WORLD.')
 
-var answer = prompt ("YOU WANT TO BE ELEGANC")
+var answer = prompt ("YOU WANT TO BE ELEGANC?")
 
-if(answer == "yes"){
-  confirm("SO FOLLOW US. ")
-  }
+if(answer == "yes"){confirm("SO FOLLOW US. ")}
 
-else if(answer == "no"){
-  confirm("BELIVE ME,YOU LIKE TO BE HERE" )
+else if(answer == "no"){confirm("BELIVE ME,YOU LIKE TO BE HERE" )
   }
-function img1(){
-var numberofimg = prompt("How beautiful do you see yourself out of 10?")
+}
+function numberofimg(){
+var numberofimg =prompt("How beautiful do you see yourself out of 10?")
 while (numberofimg >= 10){
 numberofimg=prompt("How beautiful do you see yourself out of 10?")
 }
-function img2(){
 for(var i=0;i<numberofimg;i++){
 document.write("<img ='img' src='https://media.istockphoto.com/vectors/funny-lettering-illustration-of-youre-gorgeous-vector-id1186676882' />" )
-} 
 }
 }
-console.log('img (1;2)')
+
+start();
+numberofimg();
